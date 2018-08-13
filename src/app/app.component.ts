5a35985b7ac77ae1237d1c77c7e4d6db0d86e8c3
Jaqueline Rodrigues <grodrigues.jaqueline@gmail.com>
2018-08-12 23:45:53 -0300

    falta css e submit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,12 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'app';
+  registerForm: FormGroup;
+  submitted: false;
 
-
-  constructor() {
+  constructor(private formBuilder: FormBuilder) {
   }
 
   ngOnInit() {
+  this.registerForm = this.formBuilder.group({
+    nome: ['', Validators.required],
+    telefone: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    idade: ['', [Validators.required, Validators.maxLength(3)]],
+    cidade: ['', Validators.required],
+    estado: ['', Validators.required],
+    usuario: ['', Validators.required],
+    senha: ['', [Validators.required, Validators.minLength(6)]]
+  });
   }
 
+     // convenience getter for easy access to form fields
+     get f() { return this.registerForm.controls; }
+     onSubmit() {
+        /* this.submitted = true;
+         // stop here if form is invalid
+         if (this.registerForm.invalid) {
+             return;
+         }
+         alert('SUCCESS!! :-)');*/
+  }
 }
