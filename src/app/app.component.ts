eb5ce05b6de042246663e849e938ca4ca5d94632
Jaqueline Rodrigues <grodrigues.jaqueline@gmail.com>
2018-08-15 11:30:29 -0300

    tentando validar RISOS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,20 +11,44 @@ export class AppComponent implements OnInit {
   registerForm: FormGroup;
   submitted = false;
 
-  constructor(public formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder) {
   }
 
   ngOnInit() {
-  this.registerForm = this.formBuilder.group({
-    nome: ['', Validators.required],
-    telefone: ['', Validators.required],
-    email: ['', [Validators.required, Validators.email]],
-    idade: ['', [Validators.required, Validators.maxLength(3)]],
-    cidade: ['', Validators.required],
-    estado: ['', Validators.required],
-    usuario: ['', Validators.required],
-    senha: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(10)]]
-  });
+    this.registerForm = this.formBuilder.group({
+      nome: ['', Validators.required],
+      telefone: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      idade: ['', [Validators.required, Validators.maxLength(3)]],
+      cidade: ['', Validators.required],
+      estado: ['', Validators.required],
+      usuario: ['', Validators.required],
+      senha: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(10)]]
+    });
+  }
+  get nome(){
+    return this.registerForm.get('nome');
+  }
+  get telefone(){
+    return this.registerForm.get('telefone');
+  }
+  get email() {
+    return this.registerForm.get('email');
+  }
+  get idade(){
+    return this.registerForm.get('idade');
+  }
+  get cidade(){
+    return this.registerForm.get('cidade');
+  }
+  get estado(){
+    return this.registerForm.get('estado');
+  }
+  get usuario(){
+    return this.registerForm.get('usuario');
+  }
+  get senha(){
+    return this.registerForm.get('senha');
   }
 
      onSubmit() {
